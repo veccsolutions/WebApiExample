7b283ff04453528f5662f1130d035122b48a84d6
Edward Cooke <edward@veccsolutions.com>
2015-09-04 21:46:13 -0600

    Create query API controller

---
+++ src/WebApiExample.ApiApp/Controllers/QueryController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using WebApiExample.Contracts;
+
+namespace WebApiExample.ApiApp.Controllers
+{
+    [Route("Account")]
+    public class QueryController : Controller
+    {
+        [HttpPost]
+        public Task<QueryResult> Post([FromBody]Query query)
+        {
+            return Task.FromResult(new QueryResult { Results = query.Filters.Select(x => x.GetType().ToString()).ToArray() });
+        }
+    }
+}
